c15be5944517d1eb01d824d394158e277bda835a
Esmot Ara <aesmot@gmail.com>
2023-09-24 10:21:03 +0600

    Couter with cart page

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 
-// Define a Product class to represent individual products
+void main() {
+  runApp(MaterialApp(
+    home: ProductList(),
+  ));
+}
+
 class Product {
   final String name;
   final double price;
+  int quantity;
 
-  Product(this.name, this.price);
+  Product(this.name, this.price, this.quantity);
 }
 
 class ProductList extends StatefulWidget {
@@ -14,62 +20,117 @@ class ProductList extends StatefulWidget {
 }
 
 class _ProductListState extends State<ProductList> {
-  // Create a list of products
   List<Product> products = [
-    Product('Product 1', 19.99),
-    Product('Product 2', 29.99),
-    Product('Product 3', 39.99),
-    Product('Product 4', 49.99),
-    Product('Product 5', 59.99),
-    Product('Product 6', 69.99),
-
+    Product('Product 1', 19.99, 0),
+    Product('Product 2', 29.99, 0),
+    Product('Product 3', 39.99, 0),
+    // Add more products as needed
   ];
+
+  int totalProductsBought = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Product List'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) =>
+                      CartPage(products, totalProductsBought),
+                ),
+              );
+            },
+          ),
+        ],
       ),
-      body: ListView.builder(
-        itemCount: products.length,
-        itemBuilder: (context, index) {
-          final product = products[index];
-          return ListTile(
-            title: Text(product.name),
-            subtitle: Text('\$${product.price.toStringAsFixed(2)}'),
-            trailing: ElevatedButton(
-              onPressed: () {
-                // Implement the buy now action for the product
-                showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return AlertDialog(
-                      title: Text('Buy Now'),
-                      content: Text('You have purchased {product.name}.'),
-                      actions: [
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                          child: Text('OK'),
-                        ),
-                      ],
-                    );
-                  },
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: products.length,
+              itemBuilder: (context, index) {
+                final product = products[index];
+                return ListTile(
+                  title: Text(product.name),
+                  subtitle: Text('\$${product.price.toStringAsFixed(2)}'),
+                  trailing: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('Quantity: ${product.quantity}'),
+                      ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            product.quantity++;
+                            if (product.quantity == 5) {
+                              showDialog(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return AlertDialog(
+                                    title: Text('Congratulations!'),
+                                    content: Text(
+                                        'You\'ve bought 5 ${product.name}!'),
+                                    actions: [
+                                      TextButton(
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                        child: Text('OK'),
+                                      ),
+                                    ],
+                                  );
+                                },
+                              );
+                            }
+                            totalProductsBought++;
+                          });
+                        },
+                        child: Text('Buy Now'),
+                      ),
+                    ],
+                  ),
                 );
               },
-              child: Text('Buy Now'),
             ),
-          );
-        },
+          ),
+        ],
       ),
     );
   }
 }
 
-void main() {
-  runApp(MaterialApp(
-    home: ProductList(),
-  ));
+class CartPage extends StatelessWidget {
+  final List<Product> products;
+  final int totalProductsBought;
+
+  CartPage(this.products, this.totalProductsBought);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cart'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Total Products Bought: $totalProductsBought'),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Back to Product List'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
