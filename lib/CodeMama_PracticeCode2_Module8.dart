1fc35790be18ea2a27f60af6b2bac9da2c23b3cb
Esmot Ara <aesmot@gmail.com>
2023-09-24 11:03:12 +0600

    PracticeCode2

---
+++ lib/CodeMama_PracticeCode2_Module8.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+void main() {
+  String vals = stdin.readLineSync()!;
+  //var split = vals.split(' ');
+  var productID = split[0];
+  if(productID == '101'){
+    var res = int.parse(split[1]) * 10 ;
+    print("${res}");
+  }
+  if(productID == '202'){
+    var res = int.parse(split[1]) * 25 ;
+    print("${res}");
+  }
+
+
+}
